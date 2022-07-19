eaf447b166dc5df4975e686f1e2d344a6f482577
vijjini19 <107198946+vijjini19@users.noreply.github.com>
2022-07-19 22:22:29 +0530

    Add files via upload

---
+++ Numpy_Maths.py
@@ -0,0 +1,88 @@
+import numpy as np
+# a=np.array([[20,30,40],
+# [50,60,70]])
+# b=np.array([[1,2,3],
+# [4,5,6]])
+# c=a+b
+# print(c)
+# d=a-b
+# print(d)
+# e=a*b
+# print(e)
+# f=2*a
+# print(f)
+
+# g=np.eye(3)
+# print(g)
+# g=np.eye(4)
+# print(g)
+
+# h=np.eye(3,dtype=int)
+# print(h)
+# h=np.eye(3,dtype=bool)
+# print(h)
+# h=np.eye(3,dtype=float)
+# print(h)
+
+# h=np.array([x for x in range(9)])
+# # print(h)
+# i=h.reshape((3,3))
+# print(i)
+
+# j=np.array([[2.5, 3.8, 1.5],
+#             [4.7, 2.9, 1.56]])
+# k=j.astype("int")
+# print(k)
+
+# l=np.array([[1, 0, 1],
+#             [0, 1, 0],
+#             [1, 0, 1]])
+# k=l.astype("bool")
+# print(k)
+# t=l.astype("float")
+# print(t)
+
+# n=np.array([[1,2,3],
+#             [4,5,6]])
+# o=np.array([[7,8,9],
+#             [10,11,12]])
+# p=np.hstack((n,o))
+# print(p)
+
+# n=np.array([[1,2],
+#             [4,5]])
+# o=np.array([[7,8],
+#             [10,11]])
+# p=np.vstack((n,o))
+# print(p)
+
+# a=[x for x in range(0,101,2)]
+# n=np.array(a)
+# print(n)
+
+# v=np.arange(0,101,2)
+# print(v)
+
+# w=np.array([1,2,3,4,5])
+# x=np.array([1,3,2,4,5])
+# print(np.where(w==x))
+
+# a=np.full((3,3),5)
+# print(a)
+
+# a=np.array([[1,2,3],
+#             [4,5,6],
+#             [7,8,9]])
+# b=np.array([[2,2,3],
+#             [4,6,6],
+#             [7,8,9]])
+# c=a@b
+# print(c)
+# d=np.matmul(a,b)
+# print(d)
+
+# e=np.array([[1,2,3],
+#             [4,5,6],
+#             [7,8,9]])
+# f=e.T
+# print(f)
